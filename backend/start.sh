7fee7b83b56dfafe797876d4a4cb51e100999e78
MoeJoe03 <oneeyed211@gmail.com>
2026-04-26 02:11:24 +0200

    fix crashing on backend

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 1. Start the Celery worker in the background (the '&' symbol does this)
-celery -A worker worker --loglevel=info &
+# 1. Start Celery with ONLY 1 worker to save memory
+celery -A worker worker --loglevel=info --concurrency=1 &
 
-# 2. Start the FastAPI web server in the foreground
+# 2. Start the FastAPI web server
 uvicorn main:app --host 0.0.0.0 --port $PORT
